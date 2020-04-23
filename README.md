a2839e107362aa54290eac5dd39d437dd935beca
ScorpioT1000 <scorpiot1000@yandex.ru>
2020-04-23 21:08:21 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# lua-inputMovement
-Lua inputMovement
+Lua inputMovement 
+
+## Installation
+
+### Copy code from /src and use inputMovement global
+
+### *OR* use [WLPM](https://github.com/Indaxia/wc3-wlpm-module-manager)
+```
+wlpm install https://github.com/Indaxia/lua-inputMovement
+```
+
+## Usage
+
+```lua
+
+
+```
---
+++ src/inputMovement.lua
@@ -0,0 +1,10 @@
+if(_G["WM"] == nil) then WM = (function(m,h) h(nil,(function() end), (function(e) _G[m] = e end)) end) end -- WLPM MM fallback
+
+-- Warcraft 3 inputMovement module by ScorpioT1000 / 2020
+WM("inputMovement", function(import, export, exportDefault)
+  -- Registers input for all active players when it's imported
+  
+  
+
+  exportDefault({})
+end)
